a0cd1f574ec7a66c343a7e21e123a05deeb707be
Aung Naing Oo <aungnaingoo.mhs@gmail.com>
2020-09-18 21:02:19 +0630

    update array.js

--- src/services/array.js
+++ src/services/array.js
@@ -1,10 +1,17 @@
 export default {
     arrayRemove,
+    arrayRemoveByIndex,
     arrayRemoveByKey,
-    indexOfByKey,
-    sum,
+    clone,
     groupBy,
-    objToArray
+    indexOfByKey,
+    objToArray,
+    sort,
+    sum,
+}
+
+export function sort(array, field) {
+    return array.sort(function (a, b) { return (a[field] > b[field]) ? 1 : ((b[field] > a[field]) ? -1 : 0); })
 }
 
 export const arrayRemove = (arr, value) => {
@@ -13,6 +20,13 @@ export const arrayRemove = (arr, value) => {
 
 export const arrayRemoveByKey = (arr, key, value) => {
     return arr.filter(function (ele) { return ele[key] != value; });
+}
+
+export const arrayRemoveByIndex = (arr, index) => {
+    if (index > -1) {
+        return arr.splice(index, 1);
+    }
+    return arr;
 }
 
 export const indexOfByKey = (obj_list, key, value) => {
@@ -39,3 +53,9 @@ export const objToArray = (obj) =>
     Object.keys(obj).map(function (objkey) {
         return { "key": objkey, "list": obj[objkey] };
     });
+
+export const clone = arr => {
+    var newArr = [];
+    arr.map(item => newArr.push(item));
+    return newArr;
+};
