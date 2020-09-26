ea67f4debe4707d60e8f9513a31d2783933f0a2c
Aung Naing Oo <aungnaingoo.mhs@gmail.com>
2020-09-27 01:21:14 +0630

    change const to function in arrays

--- src/services/array.js
+++ src/services/array.js
@@ -14,47 +14,47 @@ export function sort(array, field) {
     return array.sort(function (a, b) { return (a[field] > b[field]) ? 1 : ((b[field] > a[field]) ? -1 : 0); })
 }
 
-export const arrayRemove = (arr, value) => {
+export function arrayRemove(arr, value) {
     return arr.filter(function (ele) { return ele != value; });
 }
 
-export const arrayRemoveByKey = (arr, key, value) => {
+export function arrayRemoveByKey(arr, key, value) {
     return arr.filter(function (ele) { return ele[key] != value; });
 }
 
-export const arrayRemoveByIndex = (arr, index) => {
+export function arrayRemoveByIndex(arr, index) {
     if (index > -1) {
         return arr.splice(index, 1);
     }
     return arr;
 }
 
-export const indexOfByKey = (obj_list, key, value) => {
+export function indexOfByKey(obj_list, key, value) {
     for (var index = 0; index < obj_list.length; index++) {
         if (obj_list[index][key] === value) return index;
     }
     return -1;
 }
 
-export const sum = (arr, key) => {
+export function sum(arr, key) {
     return arr.reduce((a, b) => {
         return a[key] + b[key]
     })
 }
 
-export const groupBy = function (xs, key) {
+export function groupBy(xs, key) {
     return xs.reduce(function (rv, x) {
         (rv[x[key]] = rv[x[key]] || []).push(x);
         return rv;
     }, {});
 };
 
-export const objToArray = (obj) =>
-    Object.keys(obj).map(function (objkey) {
+export function objToArray(obj) {
+    return Object.keys(obj).map(function (objkey) {
         return { "key": objkey, "list": obj[objkey] };
     });
-
-export const clone = arr => {
+}
+export function clone(arr) {
     var newArr = [];
     arr.map(item => newArr.push(item));
     return newArr;
